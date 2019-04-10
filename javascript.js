b16521780feba02db159df0f67a5b7d95ac4e930
manuel-rahm <45931464+manuel-rahm@users.noreply.github.com>
2019-04-10 09:32:46 +0200

    Some changes

--- javascript.js
+++ javascript.js
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 $("tab-list").each(function(){
     var $this   = $(this);
     var $tab    = $this.find("li.active");
@@ -17,4 +18,25 @@ $("tab-list").each(function(){
             $tab = $link.parent().addClass("active");
         }
     });
+=======
+$("tab-list").each(function(){
+    var $this   = $(this);
+    var $tab    = $this.find("li.active");
+    var $link   = $tab.find("a");
+    var $panel  = $($link.attr("href"));
+
+    $this.on("click", ".tab-control", function(e){
+        e.preventDefault();
+        var $link = $(this);
+        var id  = this.hash;
+
+        if (id && !$link.is(".active")){
+            $panel.removeClass("active");
+            $tab.removeClass("active");
+
+            $panel = $(id).addClass("active");
+            $tab = $link.parent().addClass("active");
+        }
+    });
+>>>>>>> 702d21d33e4715b4e634510affa5b80f6ceae75b
 });
