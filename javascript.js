ea7295daf58e742d839206a3d9f9b65952624aa3
manuel-rahm <45931464+manuel-rahm@users.noreply.github.com>
2019-04-10 09:49:46 +0200

    changes

--- javascript.js
+++ javascript.js
@@ -1,42 +1,13 @@
-<<<<<<< HEAD
-$("tab-list").each(function(){
-    var $this   = $(this);
-    var $tab    = $this.find("li.active");
-    var $link   = $tab.find("a");
-    var $panel  = $($link.attr("href"));
-
-    $this.on("click", ".tab-control", function(e){
-        e.preventDefault();
-        var $link = $(this);
-        var id  = this.hash;
-
-        if (id && !$link.is(".active")){
-            $panel.removeClass("active");
-            $tab.removeClass("active");
-
-            $panel = $(id).addClass("active");
-            $tab = $link.parent().addClass("active");
-        }
-    });
-=======
-$("tab-list").each(function(){
-    var $this   = $(this);
-    var $tab    = $this.find("li.active");
-    var $link   = $tab.find("a");
-    var $panel  = $($link.attr("href"));
-
-    $this.on("click", ".tab-control", function(e){
-        e.preventDefault();
-        var $link = $(this);
-        var id  = this.hash;
-
-        if (id && !$link.is(".active")){
-            $panel.removeClass("active");
-            $tab.removeClass("active");
-
-            $panel = $(id).addClass("active");
-            $tab = $link.parent().addClass("active");
-        }
-    });
->>>>>>> 702d21d33e4715b4e634510affa5b80f6ceae75b
+$(document).ready(function(){
+    var bereich = $('div.tabs > div');
+     
+    $('div.tabs ul.navi a').click(function () {
+        bereich.hide().filter(this.hash).show();
+         
+        $('div.tabs ul.navi a').removeClass('selected');
+        $(this).addClass('selected');
+         
+        return false;
+    }).filter(':first').click();
 });
+
